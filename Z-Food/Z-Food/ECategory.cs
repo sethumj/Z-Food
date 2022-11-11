991879ba04feb318899a8b6e4b0b95ecd7e8da74
sethumj <54964831+sethumj@users.noreply.github.com>
2022-11-11 16:43:43 +0530

    Add files via upload

---
+++ Z-Food/Z-Food/ECategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Z_Food
+{
+    public enum Category
+    {
+        Soup,
+        Starter,
+        MainCourse,
+        Breads,
+        RiceVarieties,
+        SeaFood,
+        FriedRice,
+        Noodles,
+        Chinese,
+        Burgers,
+        Pizzas,
+        Rolls,
+        Sides,
+        Beverages,
+        Desserts
+    }
+}
+
